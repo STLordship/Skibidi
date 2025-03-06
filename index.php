91518e2dfe6e93efd98220bd37a1528b2630ab12
Georgi20310 <92362367+Georgi20310@users.noreply.github.com>
2025-03-06 11:35:08 +0200

    index.php

--- index.php
+++ index.php
@@ -15,13 +15,13 @@
     <div id="menu">
         <div class="menu-container">
             <div class="menu-section">
-                <h2>Appetizers</h2>
+                    <h2>Appetizers</h2>
                 <?php foreach ($appetizers as $dish): ?>
                 <div class="menu-item">
-                    <img src="../Сайт/images/Appetizer.jpg" alt="">
+                    <a href="../Сайт/appetizers.php"><img src="../Сайт/images/Appetizer.jpg" alt=""></a>
                     <h3><?php echo htmlspecialchars($dish['name']); ?></h3>
                     <p>Gramage: <?php echo htmlspecialchars($dish['gramage']); ?>g</p>
-                    <p class="price">Price: $<?php echo htmlspecialchars($dish['price']); ?></p>
+                    <p class="price">Price: $<?php echo number_format($dish['price'], 2); ?></p>
                 </div>
                 <?php endforeach; ?>
             </div>
@@ -30,25 +30,27 @@
                 <h2>Soups</h2>
                 <?php foreach ($soups as $dish): ?>
                 <div class="menu-item">
-                    <img src="../Сайт/images/soup.webp" alt="">
+                    <a href="../Сайт/soups.php"><img src="../Сайт/images/soup.webp" alt=""></a>
                     <h3><?php echo htmlspecialchars($dish['name']); ?></h3>
                     <p>Gramage: <?php echo htmlspecialchars($dish['gramage']); ?>g</p>
-                    <p class="price">Price: $<?php echo htmlspecialchars($dish['price']); ?></p>
+                    <p class="price">Price: $<?php echo number_format($dish['price'], 2); ?></p>
                 </div>
                 <?php endforeach; ?>
+            </a>
             </div>
 
             <div class="menu-section">
                 <h2>Main Courses</h2>
                 <?php foreach ($main_courses as $dish): ?>
                 <div class="menu-item">
-                    <img src="../Сайт/images/steak.jpg" alt="">
+                    <a href="/main_course.php"><img src="../Сайт/images/steak.jpg" alt=""></a>
                     <h3><?php echo htmlspecialchars($dish['name']); ?></h3>
                     <p>Gramage: <?php echo htmlspecialchars($dish['gramage']); ?>g</p>
-                    <p class="price">Price: $<?php echo htmlspecialchars($dish['price']); ?></p>
+                    <p class="price">Price: $<?php echo number_format($dish['price'], 2); ?></p>
                 </div>
                 <?php endforeach; ?>
             </div>
+            </a>
         </div>
     </div>
     <script src="script.js"></script>
